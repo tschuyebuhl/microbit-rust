1bf7fe225e817d9a4f91d5a37e77427c04dfa6f0
Ksawery <pyoxa@pyoxa.pl>
2024-05-18 18:25:56 +0200

    blinky with hal

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,23 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use embedded_hal::digital::{OutputPin, PinState};
+use hal::{gpio::Level, pac};
+use nrf52833_hal as hal;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    const GPIO0_PINCNF21_ROW1_ADDR: *mut u32 = 0x5000_0754 as *mut u32;
-    const GPIO0_PINCNF28_COL1_ADDR: *mut u32 = 0x5000_0770 as *mut u32;
-    const DIR_OUTPUT_POS: u32 = 0;
-    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
-    unsafe {
-        //*GPIO0_PINCNF21_ROW1_ADDR = PINCNF_DRIVE_LED;
-        //*GPIO0_PINCNF28_COL1_ADDR = PINCNF_DRIVE_LED;
-        write_volatile(GPIO0_PINCNF21_ROW1_ADDR, PINCNF_DRIVE_LED);
-        write_volatile(GPIO0_PINCNF28_COL1_ADDR, PINCNF_DRIVE_LED);
-    }
-    const GPIO_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
-    const GPIO0_OUT_ROW1_POS: u32 = 21;
+    let p = pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
+    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
     let mut is_on = false;
     loop {
-        unsafe {
-            write_volatile(GPIO_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
-        }
-        for _ in 0..400_000 {
+        _ = row1.set_state(PinState::from(is_on));
+        for _ in 0..100_000_ {
             nop();
         }
         is_on = !is_on;
